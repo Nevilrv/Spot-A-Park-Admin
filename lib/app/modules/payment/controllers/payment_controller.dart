0743aa0d4585c5ebe3c8cec118906b22ac34dfbf
Nevil <nevilrv@gmail.com>
2024-09-10 14:11:22 +0530

    Fix payment issue

--- lib/app/modules/payment/controllers/payment_controller.dart
+++ lib/app/modules/payment/controllers/payment_controller.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:admin_panel/app/modules/facilities/views/facilities_view.dart';
 import 'package:admin_panel/app/modules/payment/models/payment_model.dart';
 import 'package:admin_panel/app/services/firebase/payment_firebase_requests.dart';
+import 'package:admin_panel/app/utils/collection_name.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -35,42 +39,42 @@ class PaymentController extends GetxController {
   Rx<Status> isPaypalSandBox = Status.active.obs;
   Rx<Status> isStripeSandBox = Status.active.obs;
 
-  getPaymentData() {
+  getPaymentData() async {
+    await initPayment();
     getPayment().then((value) {
       if (value != null) {
         paymentModel.value = value;
-        //paypal
-        paypalNameController.value.text = paymentModel.value.paypal!.name!;
+        paypalNameController.value.text = paymentModel.value.paypal?.name ?? "";
         paypalClientIdController.value.text =
-            paymentModel.value.paypal!.paypalClient!;
+            paymentModel.value.paypal?.paypalClient ?? "";
         paypalSecretIdController.value.text =
-            paymentModel.value.paypal!.paypalSecret!;
-        isPaypalActive.value = paymentModel.value.paypal!.enable == true
+            paymentModel.value.paypal?.paypalSecret ?? "";
+        isPaypalActive.value = paymentModel.value.paypal?.enable == true
             ? Status.active
             : Status.inactive;
-        isPaypalSandBox.value = paymentModel.value.paypal!.isSandbox == true
+        isPaypalSandBox.value = paymentModel.value.paypal?.isSandbox == true
             ? Status.active
             : Status.inactive;
         //stripe
-        stripeNameController.value.text = paymentModel.value.strip!.name!;
+        stripeNameController.value.text = paymentModel.value.strip?.name ?? "";
         stripePublishIdController.value.text =
-            paymentModel.value.strip!.clientpublishableKey!;
+            paymentModel.value.strip?.clientpublishableKey ?? "";
         stripeSecretIdController.value.text =
-            paymentModel.value.strip!.stripeSecret!;
-        isStripeActive.value = paymentModel.value.strip!.enable == true
+            paymentModel.value.strip?.stripeSecret ?? "";
+        isStripeActive.value = paymentModel.value.strip?.enable == true
             ? Status.active
             : Status.inactive;
-        isStripeSandBox.value = paymentModel.value.strip!.isSandbox == true
+        isStripeSandBox.value = paymentModel.value.strip?.isSandbox == true
             ? Status.active
             : Status.inactive;
         //cash
-        cashNameController.value.text = paymentModel.value.cash!.name!;
-        isCashActive.value = paymentModel.value.cash!.enable == true
+        cashNameController.value.text = paymentModel.value.cash?.name ?? "";
+        isCashActive.value = paymentModel.value.cash?.enable == true
             ? Status.active
             : Status.inactive;
         //wallet
-        walletNameController.value.text = paymentModel.value.wallet!.name!;
-        isWalletActive.value = paymentModel.value.wallet!.enable == true
+        walletNameController.value.text = paymentModel.value.wallet?.name ?? "";
+        isWalletActive.value = paymentModel.value.wallet?.enable == true
             ? Status.active
             : Status.inactive;
       }
--- lib/app/services/firebase/payment_firebase_requests.dart
+++ lib/app/services/firebase/payment_firebase_requests.dart
@@ -4,6 +4,39 @@ import 'package:admin_panel/app/modules/payment/models/payment_model.dart';
 import 'package:admin_panel/app/utils/collection_name.dart';
 import 'package:admin_panel/app/utils/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+Future<void> initPayment() async {
+  bool val = await FirebaseFirestore.instance
+      .collection(CollectionName.settings)
+      .doc("payment")
+      .get()
+      .then((value) => value.exists);
+  if (!val) {
+    Map<String, dynamic> body = {
+      "wallet": {"enable": false, "name": ""},
+      "cash": {"name": "", "enable": false},
+      "paypal": {
+        "name": "",
+        "isSandbox": false,
+        "paypalClient": "",
+        "enable": false,
+        "paypalSecret": "",
+      },
+      "strip": {
+        "clientpublishableKey": "",
+        "enable": false,
+        "isSandbox": false,
+        "name": "",
+        "stripeSecret": "",
+      }
+    };
+    FirebaseFirestore.instance
+        .collection(CollectionName.settings)
+        .doc("payment")
+        .set(body);
+  }
+}
 
 Future<PaymentModel?> getPayment() async {
   PaymentModel? paymentModel;
@@ -23,7 +56,7 @@ Future<PaymentModel?> getPayment() async {
   return paymentModel;
 }
 
-Future<bool> setPayment(PaymentModel paymentModel) {
+Future<bool> setPayment(PaymentModel paymentModel) async {
   return FirebaseFirestore.instance
       .collection(CollectionName.settings)
       .doc("payment")
